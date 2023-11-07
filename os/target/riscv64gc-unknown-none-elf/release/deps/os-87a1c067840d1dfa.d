4f463d4a3908e53c8a37fd18fd6e4ee900fabce3
LiuMai <21301043@bjtu.edu.cn>
2023-11-07 21:17:36 +0800

    Initialize

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d
@@ -0,0 +1,5 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa: src/main.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d: src/main.rs
+
+src/main.rs:
