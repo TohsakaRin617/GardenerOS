65694510bd3455f30a7c246d0d24599482ffe259
LiuMai <21301043@bjtu.edu.cn>
2023-11-09 08:16:10 +0800

    Experiment2 last 21301043

--- os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d
@@ -1,5 +1,9 @@
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-87a1c067840d1dfa.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
 src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
